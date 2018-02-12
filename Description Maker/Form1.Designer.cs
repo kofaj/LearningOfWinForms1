432f52e17111279204003ec3efefd41f7030162c
kofaj <Kowallukasz01@gmail.com>
2018-02-12 20:49:21 +0100

    Creating New project

--- Description Maker/Form1.Designer.cs
+++ Description Maker/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CreateProjectButton = new System.Windows.Forms.Button();
+            this.newProjectNameLabel = new System.Windows.Forms.Label();
+            this.projectNameBox = new System.Windows.Forms.TextBox();
+            this.ListOfGeneratedDescriptions = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PDFSaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreateProjectButton
+            // 
+            this.CreateProjectButton.Location = new System.Drawing.Point(321, 224);
+            this.CreateProjectButton.Name = "CreateProjectButton";
+            this.CreateProjectButton.Size = new System.Drawing.Size(107, 23);
+            this.CreateProjectButton.TabIndex = 22;
+            this.CreateProjectButton.Text = "Create project";
+            this.CreateProjectButton.UseVisualStyleBackColor = true;
+            // 
+            // newProjectNameLabel
+            // 
+            this.newProjectNameLabel.AutoSize = true;
+            this.newProjectNameLabel.Location = new System.Drawing.Point(347, 57);
+            this.newProjectNameLabel.Name = "newProjectNameLabel";
+            this.newProjectNameLabel.Size = new System.Drawing.Size(81, 13);
+            this.newProjectNameLabel.TabIndex = 21;
+            this.newProjectNameLabel.Text = "Nazwa projektu";
+            // 
+            // projectNameBox
+            // 
+            this.projectNameBox.Location = new System.Drawing.Point(434, 57);
+            this.projectNameBox.Name = "projectNameBox";
+            this.projectNameBox.Size = new System.Drawing.Size(120, 20);
+            this.projectNameBox.TabIndex = 20;
+            // 
+            // ListOfGeneratedDescriptions
+            // 
+            this.ListOfGeneratedDescriptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ListOfGeneratedDescriptions.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ListOfGeneratedDescriptions.FormattingEnabled = true;
+            this.ListOfGeneratedDescriptions.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.ListOfGeneratedDescriptions.Location = new System.Drawing.Point(153, 53);
+            this.ListOfGeneratedDescriptions.Name = "ListOfGeneratedDescriptions";
+            this.ListOfGeneratedDescriptions.Size = new System.Drawing.Size(121, 23);
+            this.ListOfGeneratedDescriptions.Sorted = true;
+            this.ListOfGeneratedDescriptions.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Ilość wierszy z opisami";
+            // 
+            // PDFSaveButton
+            // 
+            this.PDFSaveButton.Location = new System.Drawing.Point(436, 224);
+            this.PDFSaveButton.Name = "PDFSaveButton";
+            this.PDFSaveButton.Size = new System.Drawing.Size(107, 23);
+            this.PDFSaveButton.TabIndex = 17;
+            this.PDFSaveButton.Text = "Save to PDF";
+            this.PDFSaveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 268);
+            this.Controls.Add(this.CreateProjectButton);
+            this.Controls.Add(this.newProjectNameLabel);
+            this.Controls.Add(this.projectNameBox);
+            this.Controls.Add(this.ListOfGeneratedDescriptions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PDFSaveButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CreateProjectButton;
+        private System.Windows.Forms.Label newProjectNameLabel;
+        private System.Windows.Forms.TextBox projectNameBox;
+        private System.Windows.Forms.ComboBox ListOfGeneratedDescriptions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PDFSaveButton;
     }
 }
 
